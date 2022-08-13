a2d6c61d8a9c5f75270290601101c6bef5133ed4
imlaa <bohdanvoznyi.em@gmail.com>
2022-08-13 16:19:28 +0300

    created container div

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const App:React.FC = () => {
 	return (
-		<div className="App">
-
+		<div className="w-screen h-full">
+			<div className="w-[1200px] my-0 mx-auto">
+			</div>
 		</div>
 	);
 }
