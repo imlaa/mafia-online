efd71e598ecab58c7264afbdcf723a953a827d9b
imlaa <bohdanvoznyi.em@gmail.com>
2022-08-23 08:26:40 +0300

    created temporary logo

---
+++ client/src/components/logo/logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './logo.module.scss';
+
+export const Logo:React.FC = () => {
+	return (
+		<div className={classes.logo}>
+			MAFIA
+		</div>
+	);
+};
