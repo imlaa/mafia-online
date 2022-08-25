6ada66439369934e06936667fd97851de0a8efc7
imlaa <bohdanvoznyi.em@gmail.com>
2022-08-25 09:06:17 +0300

    created routes structure

---
+++ client/src/routes/routes.enum.ts
@@ -0,0 +1,8 @@
+export enum RoutesEnum {
+	HOME = '/',
+	RULES = '/rules',
+	ACCOUNT = '/account',
+
+	CREATE_ROOM = '/create-room',
+	ROOM = '/room:',
+}
---
+++ client/src/routes/routes.ts
@@ -0,0 +1,5 @@
+import { IRoute } from '../models';
+
+export const publicRoutes:IRoute[] = [
+
+];
