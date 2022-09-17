b32d40758548306f31b7f376838b12245dc81f24
imlaa <bohdanvoznyi.em@gmail.com>
2022-09-17 10:42:03 +0300

    configured aliases

--- client/craco.config.ts
+++ client/craco.config.ts
@@ -5,6 +5,11 @@ module.exports = {
 	webpack: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
+			'@assets': path.resolve(__dirname, 'src', 'assets'),
+			'@components': path.resolve(__dirname, 'src', 'components'),
+			'@models': path.resolve(__dirname, 'src', 'models'),
+			'@modules': path.resolve(__dirname, 'src', 'modules'),
+			'@routes': path.resolve(__dirname, 'src', 'routes'),
 		},
 	},
 };
